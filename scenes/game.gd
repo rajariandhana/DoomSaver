b0838631ab5a4d16125e082411c1613c1c76ace0
Ralfazza Rajariandhana <s4949323@student.uq.edu.au>
2025-09-21 02:33:28 +1000

    setup

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,39 +1,29 @@
 extends Node2D
 
-var last_screen := -1
+@onready var character: Node2D = $Character
 
 func _ready():
 	_apply_transparent_pet_flags()
 	_center_on_screen()
-
-func _process(_delta):
 	pass
-	#var current_screen = get_window().current_screen
-	#if current_screen != last_screen:
-		#last_screen = current_screen
-		#_center_on_screen()
+
+#func _process(_delta):
+	#pass
 		
-func _on_screen_changed():
-	print("Screen changed:", get_window().current_screen)
-	_center_on_screen()
-
-func _on_window_size_changed():
-	print("Window size changed:", get_window().size)
-	_center_on_screen()
-
 func _center_on_screen():
 	var screen = get_window().current_screen
 	var screen_size = DisplayServer.screen_get_size(screen)
 	var window_size = DisplayServer.window_get_size()
 	var pos = (screen_size - window_size) / 2
 	DisplayServer.window_set_position(pos, screen)
-	
+
 func _apply_transparent_pet_flags():
-	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, false)
-	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
-	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_ALWAYS_ON_TOP, false)
-	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_MOUSE_PASSTHROUGH, false)
+
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, true)
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_ALWAYS_ON_TOP, true)
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_MOUSE_PASSTHROUGH, true)
 
 	# Allow per-pixel transparency on macOS
 	#ProjectSettings.set_setting("display/window/per_pixel_transparency/allowed", true)
-	#ProjectSettings.set_setting("display/window/per_pixel_transparency/enabled", true)
+	#ProjectSettings.set_setting("rendering/viewport/transparent_background", true)
